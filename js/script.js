5c5b90e26a79ac94cd3a8d24131221606c03fb63
Zoemaestra <P2423891@my365.dmu.ac.uk>
2021-03-01 15:06:59 +0000

    User input with dark/light theme toggle from navigation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
+"use strict";
+
+let bgcolour = "#FFFFFF";
+let fgcolour = "#000000";
+
 menuToggler.addEventListener('click', ev => {
   menu.classList.toggle('open');
   menuToggler.classList.toggle('open');
+});
+
+themeToggler.addEventListener('click', ev => {
+  if (bgcolour == "#FFFFFF")
+  {
+    bgcolour = "#171720";
+    fgcolour = "#FFFFFF";
+  }
+  else
+  {
+    bgcolour = "#FFFFFF";
+    fgcolour = "#000000";
+  }
+  document.documentElement.style.setProperty('--bg-colour', bgcolour);
+  document.documentElement.style.setProperty('--fg-colour', fgcolour);
 });
