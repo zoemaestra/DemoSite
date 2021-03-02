34d5d4c105a83145e4cd363cd99b4b58c1bd03cd
Zoemaestra <P2423891@my365.dmu.ac.uk>
2021-03-02 12:56:34 +0000

    Theme transitions and navbar contrast

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 let bgcolour = "#E8E8DF";
 let fgcolour = "#171720";
+let navcolour = "#171720";
 
 menuToggler.addEventListener('click', ev => {
   menu.classList.toggle('open');
@@ -13,12 +14,15 @@ themeToggler.addEventListener('click', ev => {
   {
     bgcolour = "#171720";
     fgcolour = "#E8E8DF";
+    navcolour = "#2B2B3B";
   }
   else
   {
     bgcolour = "#E8E8DF";
     fgcolour = "#171720";
+    navcolour = "#171720";
   }
   document.documentElement.style.setProperty('--bg-colour', bgcolour);
   document.documentElement.style.setProperty('--fg-colour', fgcolour);
+  document.documentElement.style.setProperty('--nav-colour', navcolour);
 });
