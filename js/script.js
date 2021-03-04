22da8ce8e48bbcb0b4bd07f705502f7336c13beb
Zoemaestra <P2423891@my365.dmu.ac.uk>
2021-03-04 19:24:01 +0000

    Implemented local storage for persistent themes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,11 @@
 "use strict";
 
-let bgcolour = "#E8E8DF";
-let fgcolour = "#171720";
-let navcolour = "#171720";
+let bgcolour = localStorage.getItem("Theme");
+let fgcolour = "";
+let navcolour = "";
 
-menuToggler.addEventListener('click', ev => {
-  menu.classList.toggle('open');
-  menuToggler.classList.toggle('open');
-});
-
-themeToggler.addEventListener('click', ev => {
-  if (bgcolour == "#E8E8DF")
+function themeSet(theme) {
+  if (theme == "#171720")
   {
     bgcolour = "#171720";
     fgcolour = "#E8E8DF";
@@ -22,7 +17,25 @@ themeToggler.addEventListener('click', ev => {
     fgcolour = "#171720";
     navcolour = "#171720";
   }
+  bgcolour = theme;
   document.documentElement.style.setProperty('--bg-colour', bgcolour);
   document.documentElement.style.setProperty('--fg-colour', fgcolour);
   document.documentElement.style.setProperty('--nav-colour', navcolour);
+  localStorage.setItem("Theme", bgcolour);
+}
+
+themeSet(bgcolour);
+
+menuToggler.addEventListener('click', ev => {
+  menu.classList.toggle('open');
+  menuToggler.classList.toggle('open');
+});
+
+themeToggler.addEventListener('click', ev => {
+  if (bgcolour == "#171720"){themeSet("#E8E8DF");}
+  else {themeSet("#171720");}
+});
+
+window.addEventListener('storage', () => {
+  themeSet(localStorage.getItem("Theme"));
 });
