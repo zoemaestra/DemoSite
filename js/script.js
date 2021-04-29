c8edd2f493f5835bea920540b123e4ee64f5e206
Zoemaestra <P2423891@my365.dmu.ac.uk>
2021-04-29 21:52:35 +0100

    Ran a linter for intendation consistency

--- js/script.js
+++ js/script.js
@@ -8,48 +8,53 @@ let currentImage = 1;
 
 const themeSet = (theme) => {
   //Function to set the CSS colour variables used for background, foreground and nav background
-  if (theme == "#171720"){
+  if (theme == "#171720") {
     bgcolour = "#171720";
     fgcolour = "#E8E8DF";
     navcolour = "#2B2B3B";
     document.getElementById("themeToggler").innerHTML = "<a href='#'>☀️</a>";
-  }
-  else{
+  } else {
     bgcolour = "#E8E8DF";
     fgcolour = "#171720";
     navcolour = "#171720";
     document.getElementById("themeToggler").innerHTML = "<a href='#'>🌑</a>";
   }
-  document.documentElement.style.setProperty('--bg-colour', bgcolour);
-  document.documentElement.style.setProperty('--fg-colour', fgcolour);
-  document.documentElement.style.setProperty('--nav-colour', navcolour);
+  document.documentElement.style.setProperty("--bg-colour", bgcolour);
+  document.documentElement.style.setProperty("--fg-colour", fgcolour);
+  document.documentElement.style.setProperty("--nav-colour", navcolour);
   localStorage.setItem("Theme", bgcolour);
 };
 
-themeSet(bgcolour);
+document.addEventListener("DOMContentLoaded", function() {
+  //Set the theme when the content is loaded
+  themeSet(bgcolour);
+});
 
-window.addEventListener('storage', () => {
+window.addEventListener("storage", () => {
   themeSet(localStorage.getItem("Theme"));
 });
 
-menuToggler.addEventListener('click', ev => {
-  menu.classList.toggle('open');
-  menuToggler.classList.toggle('open');
+menuToggler.addEventListener("click", (ev) => {
+  menu.classList.toggle("open");
+  menuToggler.classList.toggle("open");
 });
 
-themeToggler.addEventListener('click', ev => {
-  if (bgcolour == "#171720"){themeSet("#E8E8DF");}
-  else {themeSet("#171720");}
+themeToggler.addEventListener("click", (ev) => {
+  if (bgcolour == "#171720") {
+    themeSet("#E8E8DF");
+  } else {
+    themeSet("#171720");
+  }
 });
 
-if (document.getElementById("script").getAttribute('class') != "home"){
+if (document.getElementById("script").getAttribute("class") != "home") {
   //Prevent event listeners trying to work on page without the buttons
-  galleryForwardImg.addEventListener('click', ev => {
-    gallery(1)
+  galleryForwardImg.addEventListener("click", (ev) => {
+    gallery(1);
   });
 
-  galleryBackwardImg.addEventListener('click', ev => {
-    gallery(-1)
+  galleryBackwardImg.addEventListener("click", (ev) => {
+    gallery(-1);
   });
 }
 
@@ -57,31 +62,29 @@ const gallery = (direction) => {
   //Function to change the image shown when the gallery buttons are clicked
   let max = 2;
   if (direction == 1) {
-    if (currentImage == max){
+    if (currentImage == max) {
       currentImage = 1;
-    }
-    else{
+    } else {
       currentImage++;
     }
-  }
-  else {
-    if (currentImage == 1){
+  } else {
+    if (currentImage == 1) {
       currentImage = max;
-    }
-    else{
+    } else {
       currentImage--;
     }
   }
-  document.getElementById("galleryImg").src = `images/${document.getElementById("script").getAttribute('class')}/${currentImage}.png`
+  document.getElementById("galleryImg").src = `images/${document
+    .getElementById("script")
+    .getAttribute("class")}/${currentImage}.png`;
 };
 
 const loadObject = async () => {
   //Loads information to be shown on the page from GitHub
-  if (document.getElementById("script").getAttribute('class') == "home"){
-    url = 'https://api.github.com/users/zoemaestra';
-  }
-  else{
-    url = `https://api.github.com/repos/zoemaestra/${document.currentScript.getAttribute('class')}`;
+  if (document.getElementById("script").getAttribute("class") == "home") {
+    url = "https://api.github.com/users/zoemaestra";
+  } else {
+    url = `https://api.github.com/repos/zoemaestra/${document.currentScript.getAttribute("class")}`;
   }
   let response = await fetch(url);
   return response.json();
@@ -89,14 +92,13 @@ const loadObject = async () => {
 
 const insertUserDetails = async (obj) => {
   //Inserts loaded details into the relevant page
-  if (document.getElementById("script").getAttribute('class') == "home"){
+  if (document.getElementById("script").getAttribute("class") == "home") {
     document.getElementById("headerimg").src = await obj.avatar_url;
     document.getElementById("bio").innerHTML = await obj.bio;
     document.getElementById("stats").innerHTML = await `${obj.followers} followers`;
-  }
-  else{
+  } else {
     document.getElementById("bio").innerHTML = await obj.description;
-    document.getElementById("stats").innerHTML =await `${obj.stargazers_count} stars`;
+    document.getElementById("stats").innerHTML = await `${obj.stargazers_count} stars`;
   }
   document.getElementById("gitUrl").href = await obj.html_url;
 };
